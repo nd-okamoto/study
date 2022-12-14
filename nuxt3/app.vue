cf4f6d126546b63e1b033b64f319d21c5426facb
okamoto_northdetail <ndtrokam@mediba.jp>
2022-12-14 17:00:14 +0900

    init

---
+++ nuxt3/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <v-btn class="ma-5" color="primary">my-btn</v-btn>
+    <div style="display: flex; gap: 1rem; justify-content: center;">
+      <v-btn>Vue.js</v-btn>
+      <v-btn variant="outlined">Vuetify3</v-btn>
+      <v-btn variant="tonal">Gatsby</v-btn>
+      <v-btn variant="text">React</v-btn>
+      <v-btn variant="plain">WordPress</v-btn>
+    </div>
+  </div>
+  
+</template>
