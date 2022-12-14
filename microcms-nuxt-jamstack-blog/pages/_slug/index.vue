cf2ba281f05aff5259d738320d8e070b971f328f
okamoto_northdetail <ndtrokam@mediba.jp>
2022-12-14 17:14:04 +0900

    init

---
+++ microcms-nuxt-jamstack-blog/pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="main">
+        <h1 class="title">{{ title }}</h1>
+        <p class="publishedAt">{{ publishedAt }}</p>
+        <div class="post" v-html="body"></div>
+    </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+async asyncData({ params }) {
+    const { data } = await axios.get(
+    `https://arinekoinuneko.microcms.io/api/v1/blog/${params.slug}`,
+    {
+        headers: { 'X-MICROCMS-API-KEY': '852600963aeb4afb989605367dcf60a88edc' }
+    }
+    )
+    return data
+}
+}
+</script>
